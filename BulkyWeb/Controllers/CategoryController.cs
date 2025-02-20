ea65ae7ad1d161808db4ffacabaf56fc073a5812
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-20 18:29:27 +0000

    Seed Categoriey + Get all + Display

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+
+        // if legacy .NET framework, we need to open DB connection here, then query the database, then close the connection
+        // if .NET Core, we use Entity Framework Core
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList<Category>();
+            return View(objCategoryList);
         }
     }
 }
--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,15 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // seed data
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 1, CategoryName = "Action", DisplayOrder = 1 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 2, CategoryName = "SciFi", DisplayOrder = 2 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 3, CategoryName = "History", DisplayOrder = 3 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 4, CategoryName = "Drama", DisplayOrder = 4 });
+
+        }
     }
 }
