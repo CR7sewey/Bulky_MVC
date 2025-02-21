64df51476f065d19bb4a631aa7e1faedd043fa6b
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-21 11:36:23 +0000

    Server Side Validations - Category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,10 +8,12 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; } // Primary Key - CategoryId
         [Required]
+        [MaxLength(30)] // server side validation
         [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
         [Required]
         [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "The value must be between 1 and 100.")] // server side validation
         public int DisplayOrder { get; set; }
     }
 }
