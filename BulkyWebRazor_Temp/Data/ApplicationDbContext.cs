a59b14ae0aa82cd361ed144581851df1d32f091f
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-22 16:16:21 +0000

    Razor project + Entity F. initial setup

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext: DbContext // root class of entity framework
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // seed data
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 1, CategoryName = "Action", DisplayOrder = 1 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 2, CategoryName = "SciFi", DisplayOrder = 2 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 3, CategoryName = "History", DisplayOrder = 3 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 4, CategoryName = "Drama", DisplayOrder = 4 });
+
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } // Primary Key - CategoryId
+        [Required]
+        [MaxLength(30)] // server side validation
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+        [Required]
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "The value must be between 1 and 100.")] // server side validation
+        public int DisplayOrder { get; set; }
+    }
+}
