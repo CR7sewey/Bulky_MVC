7a79d3013d7ee8a12488f5a07bded83bab3da575
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-23 10:39:26 +0000

    Post Category

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,57 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        [BindProperty] // on post binds and available on post handler
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        
+        public IActionResult OnPost()
+        {
+
+
+            
+                if (Category.CategoryName.ToLower() == Category.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("", "The category name can't be the same as Display Order");
+                }
+                // if the category exists in the database, add an error
+                var objFromDb = _db.Categories.FirstOrDefault(s => s.CategoryName == Category.CategoryName);
+                if (objFromDb != null)
+                {
+                    ModelState.AddModelError("CategoryName", "The category already exists");
+                }
+
+                if (Category.DisplayOrder <= 0)
+                {
+                    ModelState.AddModelError("DisplayOrder", "The value needs to be positive");
+                }
+                if (ModelState.IsValid)
+                {
+                _db.Categories.Add(Category); // provided and habndled by entity framework! no open and close connection, or inserts!
+                _db.SaveChanges();
+                //TempData.Add("Success", $"{objFromDb.CategoryName} created successfully");
+                TempData["Success"] = $"{Category.CategoryName} created successfully";
+                return RedirectToPage("Index");
+                }
+            return RedirectToPage("Create");
+        }
+
+    }
+}
