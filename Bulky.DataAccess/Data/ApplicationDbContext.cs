8f62936f371638448ffa90284ba3e2891bcb6e7e
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-24 16:14:55 +0000

    N-Tier Architecture setup

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
-﻿using BulkyWeb.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Bulky.Models;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
     public class ApplicationDbContext: DbContext // root class of entity framework
     {
