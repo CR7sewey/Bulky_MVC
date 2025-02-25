ada897a81e9ac017a673e3cc3a9320d757f6b7c6
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-25 11:29:35 +0000

    IRepository + Repository

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); // entity ex: Categories _db.Categories == dbSet
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> data = dbSet.Where(filter); // like an array of data
+            return data.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> data = dbSet;
+            return data.ToList();
+        }
+
+        public void Remove(T identity)
+        {
+            dbSet.Remove(identity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
