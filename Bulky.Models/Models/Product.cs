bc9b8ceae1d64d4d642804bbbcfb1ec06a384e0a
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-25 17:33:15 +0000

    Product Model update + Seed Product + Migration

--- Bulky.Models/Models/Product.cs
+++ Bulky.Models/Models/Product.cs
@@ -21,22 +21,27 @@ namespace Bulky.Models.Models
         [Required]
         public string Author { get; set; }
 
-
         [Required]
-        [Display(Name = "List Price 1-50")]
-        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 10000.")] // server side validation
-        public List<double> Price { get; set; }
-
-        [Required]
-        [Display(Name = "List Price 50+")]
-        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 10000.")] // server side validation
-        public List<double> Price50 { get; set; }
+        [Display(Name = "List Price")]
+        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 1000.")] // server side validation
+        public double ListPrice { get; set; }
 
 
         [Required]
-        [Display(Name = "List Price 100+")]
-        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 10000.")] // server side validation
-        public List<double> Price100 { get; set; }
+        [Display(Name = "Price 1-50")]
+        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 1000.")] // server side validation
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price 50+")]
+        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 1000.")] // server side validation
+        public double Price50 { get; set; }
+
+
+        [Required]
+        [Display(Name = "Price 100+")]
+        [Range(1, 1000, ErrorMessage = "The value must be between 1 and 1000.")] // server side validation
+        public double Price100 { get; set; }
 
 
 
