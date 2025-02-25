1f2c6519f0aae147dd2997596196c09e9554c4e0
CR7sewey <miguelmotacarvalho@gmail.com>
2025-02-25 11:47:44 +0000

    ICategoryRepository + CategoryRepository

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+            
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(CategoryRepository category)
+        {
+            _db.Update(category);
+        }
+    }
+}
