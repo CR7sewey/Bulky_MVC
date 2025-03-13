8f5d81e222930b2ecb7aec00d3fc584ac7aa49ba
CR7sewey <miguelmotacarvalho@gmail.com>
2025-03-13 19:30:07 +0000

    Stripe COnfiguration setup

---
+++ Bulky.Utility/StripeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    public class StripeSettings
+    {
+        public string PublishableKey { get; set; } // injection dependency from appsettings.json file (in program.cs)
+        public string SecretKey { get; set; }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(); // add dependcy injection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// stripe Injection keys
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
 // binds the IndentityUser to the EntityFramework
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.ConfigureApplicationCookie(options => // middleware for authentication and authorization, override the default path
@@ -41,7 +44,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+Stripe.StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe")["SecretKey"]; // get the secret key from the appsettings.json
 app.UseRouting();
 app.UseAuthentication(); // if username or password is valid - middleware
 app.UseAuthorization(); // acess based on role - ex Admin and Customer
