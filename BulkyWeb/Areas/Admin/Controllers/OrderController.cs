bd385b821f8301de008128a2ec429e45378fd996
CR7sewey <miguelmotacarvalho@gmail.com>
2025-03-16 11:13:58 +0000

    Status filter

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Bulky.DataAccess.Repository.IRepository;
+﻿using System.Diagnostics;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models.Models;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
@@ -27,13 +28,27 @@ namespace BulkyWeb.Areas.Admin.Controllers
         // https://datatables.net/manual/ajax
         // endpoint to get all products - api
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> orders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        
+            List<OrderHeader> orders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList(); 
+            Dictionary<string,string> keyValuePairs = new Dictionary<string, string>()
+            {
+                {"pending", SD.Status_Pending},
+                {"inprocess", SD.Status_InProcess},
+                {"completed", SD.Status_Shipped},
+                {"approved", SD.Status_Approved},                
+            };
+            if (status != "all" && status != null)
+            {
+                orders = orders.Where(o => o.OrderStatus == keyValuePairs[status]).ToList();
+            }
             return Json(new { data = orders });
+            
+            
         }
 
-    
+
 
         #endregion
     }
---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    var url = window.location.search;
+    var url2 = new URLSearchParams(url);
+    var status = url2.get('status');
+    console.log(status);
+    if (status == null) {
+        loadDataTable(`?status=all`);
+    }
+    else {
+        loadDataTable(`?status=${status}`);
+    }
+});
+
+function loadDataTable(url) {
+    dataTable = $('#tblOrder').DataTable(
+        {
+            ajax: {
+                url: `/admin/order/getall${url}`,
+                dataSrc: 'data'
+            },
+
+            columns: [
+
+                { data: 'id', width: '20%' },
+                { data: 'name', width: '20%' },
+                { data: 'phoneNumber', width: '20%' },
+                { data: 'applicationUser.email', width: '20%' },
+                { data: 'orderStatus', width: '20%' },
+                { data: 'orderTotal', width: '20%' },
+                {
+                    data: 'id',
+                    render: function (data) {
+                        return `<div class="w-75 btn-group" role="group">
+                                    <a href="/Admin/Order/Details?orderId=${id}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+                                </div>`
+                    }
+                }
+
+            ],
+        },
+
+    );
+
+
+}
