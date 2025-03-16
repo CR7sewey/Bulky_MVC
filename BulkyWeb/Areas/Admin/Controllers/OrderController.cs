836148a07e5cd99109c9a14ebd275f016ffdd2cc
CR7sewey <miguelmotacarvalho@gmail.com>
2025-03-16 09:51:12 +0000

    OrderVM e Order Controller

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller // order controller to manage orders
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API calls
+        // https://datatables.net/manual/ajax
+        // endpoint to get all products - api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orders });
+        }
+
+    
+
+        #endregion
+    }
+}
