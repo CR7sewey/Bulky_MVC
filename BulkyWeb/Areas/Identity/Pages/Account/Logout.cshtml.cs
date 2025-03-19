11553f893ef03c1bfc4136eca476ac4c82247064
CR7sewey <miguelmotacarvalho@gmail.com>
2025-03-19 10:50:09 +0000

    View Component for Shopping Cart Session

--- BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -27,7 +27,7 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            HttpContext.Session.Clear();
+            //HttpContext.Session.Clear(); - not needed bcs of ShoppingCartViewComponent
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
---
+++ BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userID = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userID != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    var valueCount = 0;
+                    valueCount = _unitOfWork.ShoppingCart.GetAll(it => it.ApplicationUser.Id == userID.Value).ToList().Count();
+                    HttpContext.Session.SetInt32(SD.SessionCart, valueCount);
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+                    } 
+
+    }
+}
